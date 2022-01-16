642e66934363b86a9c394fb5f5f6a7a7f0f6644c
Asilbek Madiev <amadiev@ozon.ru>
2022-01-17 02:13:49 +0300

    add route task

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const cors = require('cors');
 const fs = require('fs');
 
 const mongoose = require('mongoose');
-const Task = require('./models/Task');
-const jsonParser = express.json();
-const { getSuccessResponse } = require('./constants');
+const taskRouter = require('./routes/task');
 
 // Middlewares
 dotenv.config();
@@ -49,62 +47,4 @@ app.get('/', (req, res) => {
     res.status(200).send('<h1>Main page</h1>');
 });
 
-app.get('/task/getTasks', (req, res) => {
-    Task.find({}, (error, tasks) => {
-        if (error) return res.status(500).send(error);
-
-        res.status(200).send(getSuccessResponse(tasks));
-    });
-});
-
-app.post('/task/getTaskById', jsonParser, (req, res) => {
-    const id = req.body.id;
-
-    Task.find({ _id: id }, (err, tasks) => {
-        if (err) return res.status(500).send(err);
-
-        res.status(200).send(getSuccessResponse(tasks[0]));
-    });
-});
-
-app.post('/task/addTask', jsonParser, async (req, res) => {
-    const task = new Task({
-        label: req.body.label,
-        done: req.body.done,
-        important: req.body.important,
-    });
-
-    await task.save((err) => {
-        if (err) res.status(500).send(err);
-
-        res.status(200).send(getSuccessResponse({}));
-    });
-});
-
-app.post('/task/updateTask', jsonParser, (req, res) => {
-    if (!req.body.newTask)
-        return res.status(422).send({ message: 'Отсутствует обязательный параметр newTask' });
-
-    const id = req.body.id;
-    const updatedTask = {
-        label: req.body.newTask.label,
-        done: req.body.newTask.done,
-        important: req.body.newTask.important,
-    };
-
-    Task.findByIdAndUpdate(id, updatedTask, (err) => {
-        if (err) return res.send(500, err);
-
-        res.status(200).send(getSuccessResponse({ id }));
-    });
-});
-
-app.post('/task/removeTask', jsonParser, (req, res) => {
-    const id = req.body.id;
-
-    Task.findByIdAndDelete(id, (err) => {
-        if (err) return res.send(500, err);
-
-        res.status(200).send(getSuccessResponse({ id }));
-    });
-});
+app.use(taskRouter);
---
+++ routes/task.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const Task = require('../models/Task');
+const { getSuccessResponse } = require('../constants');
+const jsonParser = express.json();
+
+const taskRouter = express.Router();
+
+taskRouter.get('/task/getTasks', (req, res) => {
+    Task.find({}, (error, tasks) => {
+        if (error) return res.status(500).send(error);
+
+        res.status(200).send(getSuccessResponse(tasks));
+    });
+});
+
+taskRouter.post('/task/getTaskById', jsonParser, (req, res) => {
+    const id = req.body.id;
+
+    Task.find({ _id: id }, (err, tasks) => {
+        if (err) return res.status(500).send(err);
+
+        res.status(200).send(getSuccessResponse(tasks[0]));
+    });
+});
+
+taskRouter.post('/task/addTask', jsonParser, async (req, res) => {
+    const task = new Task({
+        label: req.body.label,
+        done: req.body.done,
+        important: req.body.important,
+    });
+
+    await task.save((err) => {
+        if (err) res.status(500).send(err);
+
+        res.status(200).send(getSuccessResponse({}));
+    });
+});
+
+taskRouter.post('/task/updateTask', jsonParser, (req, res) => {
+    if (!req.body.newTask)
+        return res.status(422).send({ message: 'Отсутствует обязательный параметр newTask' });
+
+    const id = req.body.id;
+    const updatedTask = {
+        label: req.body.newTask.label,
+        done: req.body.newTask.done,
+        important: req.body.newTask.important,
+    };
+
+    Task.findByIdAndUpdate(id, updatedTask, (err) => {
+        if (err) return res.send(500, err);
+
+        res.status(200).send(getSuccessResponse({ id }));
+    });
+});
+
+taskRouter.post('/task/removeTask', jsonParser, (req, res) => {
+    const id = req.body.id;
+
+    Task.findByIdAndDelete(id, (err) => {
+        if (err) return res.send(500, err);
+
+        res.status(200).send(getSuccessResponse({ id }));
+    });
+});
+
+module.exports = taskRouter;
